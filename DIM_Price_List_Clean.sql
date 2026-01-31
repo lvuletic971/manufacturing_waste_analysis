dba8c50f771948c9dcbc442f76fd0dad4cbde6df
lvuletic971 <lvuletic971@gmail.com>
2026-01-31 21:51:31 +0100

    Update DIM_Price_List_Clean.sql

--- DIM_Price_List_Clean.sql
+++ DIM_Price_List_Clean.sql
@@ -1,11 +1,11 @@
 --INSERT INTO [Cenovnik]
 SELECT 
-	   CnStZapisa AS SifraC
-	  ,CnSifMp AS ArtiklC
-	  ,CAST(CnDatSpremembe AS date) AS DatumC
-	  ,CnRazTipCn AS TipCenovnikaCcc
-	  ,CnSklad AS SkladisteC
-	  ,CnCena AS CenaC
+	CnStZapisa AS SifraC,
+	CnSifMp AS ArtiklC,
+	CAST(CnDatSpremembe AS date) AS DatumC,
+	CnRazTipCn AS TipCenovnika,
+	CnSklad AS SkladisteC,
+	CnCena AS CenaC
 FROM Cenik c --to je view na KG
 WHERE c.CnRazTipCn ='4' AND c.CnDatSpremembe BETWEEN '2021-10-01' AND '2021-12-31' 
-ORDER BY DatumC DESC, ArtiklCS
+ORDER BY DatumC DESC, ArtiklC, ArtiklCS
