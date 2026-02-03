c8b2ff7e515c1234b78fa7e3c8f82df2fac1c8b2
lvuletic971 <lvuletic971@gmail.com>
2026-02-03 21:01:05 +0100

    Update DIM_Article_Prepare.sql

--- SQL/DIM_Article_Prepare.sql
+++ SQL/DIM_Article_Prepare.sql
@@ -1,3 +1,4 @@
+--Select for articles
 --INSERT INTO [KG].[dbo].[Artikli]
 SELECT 
 	MpSifra AS SifraA,
@@ -12,11 +13,24 @@ SELECT
 	MpCena AS KategorijaCenaA
 FROM MaticniPodatki
 WHERE MpSifKarKlj IN ('I', 'P') 
+	
 
 --Select for classification
-
 --INSERT INTO [KG].[dbo].[Klasifikacija]
 SELECT 
 	KLoStZapisa AS SifraKlasif,
 	KLoOpis AS OpisKlasif
 FROM KlasOzn
+
+--Select fror pricelist	
+--INSERT INTO [Cenovnik]
+SELECT 
+	CnStZapisa AS SifraC,
+	CnSifMp AS ArtiklC,
+	CAST(CnDatSpremembe AS date) AS DatumC,
+	CnRazTipCn AS TipCenovnika,
+	CnSklad AS SkladisteC,
+	CnCena AS CenaC
+FROM Cenik c --to je view na KG
+WHERE c.CnRazTipCn ='4' AND c.CnDatSpremembe BETWEEN '2021-10-01' AND '2021-12-31' 
+ORDER BY DatumC DESC, ArtiklC
