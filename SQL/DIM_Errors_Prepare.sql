26836afead27fea5148130dd5cb97ad7dabbc22c
lvuletic971 <lvuletic971@gmail.com>
2026-02-03 21:36:06 +0100

    Update and rename DIM_Errors_Prepare.sql to DIM_Error_Prepare.sql

--- SQL/DIM_Errors_Prepare.sql
+++
@@ -1,9 +0,0 @@
---Select for errors
-
---INSERT INTO [KG].[dbo].[DodatniPodaci]
-SELECT 
-	MSSifra AS SifrDP,
-	MSTipSif AS TipDP,
-	MSNaziv AS NazivDP
-FROM MaliSifranti
-WHERE MSTipSif IN ('VRN', 'VZN') 
